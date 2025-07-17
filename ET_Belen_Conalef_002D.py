0578b9856d50926d539e5cbbbf9fb54069f0ca1b
Belenxdk <be.conalef@duocuc.cl>
2025-07-17 11:03:35 -0400

    Add files via upload

---
+++ ET_Belen_Conalef_002D.py
@@ -0,0 +1,75 @@
+productos = {'8475HD': ['HP', 15.6, '8GB', 'DD', '1T', 'Intel Core i5', 'Nvidia GTX1050'],
+'2175HD': ['lenovo', 14, '4GB', 'SSD', '512GB', 'Intel Core i5', 'Nvidia GTX1050'],
+'JjfFHD': ['Asus', 14, '16GB', 'SSD', '256GB', 'Intel Core i7', 'Nvidia RTX2080Ti'],
+'fgdxFHD': ['HP', 15.6, '8GB', 'DD', '1T', 'Intel Core i3', 'integrada'],
+'GF75HD': ['Asus', 15.6, '8GB', 'DD', '1T', 'Intel Core i7', 'Nvidia GTX1050'],
+'123FHD': ['lenovo', 14, '6GB', 'DD', '1T', 'AMD Ryzen 5', 'integrada'],
+'342FHD': ['lenovo', 15.6, '8GB', 'DD', '1T', 'AMD Ryzen 7', 'Nvidia GTX1050'],
+'UWU131HD': ['Dell', 15.6, '8GB', 'DD', '1T', 'AMD Ryzen 3', 'Nvidia GTX1050'],}
+
+stock = {'8475HD': [387990,10], '2175HD': [327990,4], 'JjfFHD': [424990,1],
+'fgdxFHD': [664990,21], '123FHD': [290890,32], '342FHD': [444990,7],
+'GF75HD': [749990,2], 'UWU131HD': [349990,1], 'FS1230HD': [249990,0],}
+
+def stock_marca(marca):
+    marca = input ("Ingrese la marca a consultar : ").strip
+    marca
+     
+def busqueda_stock(stock):
+    stock
+     
+def búsqueda_precio(p_min,p_max, precio):
+    if p_min >=100000 :
+        input("Ingrese precio mínimo: ")
+    if p_max <= 800000 :
+        input("Ingrese precio máximo:  ")
+    if precio <=0 :
+        range <= 0
+        print[stock, productos[0]]
+    
+          
+          
+def actualizar_modelo(modelo):
+     modelo
+     
+def actualizar_precio(precio):
+     precio
+
+
+
+
+
+def main():
+    while True : 
+
+        print ("*** MENU PRINCIPAL ***")
+
+        print ("1. Stock marca.")
+        print ("2. Búsqueda por precio.")
+        print ("3. Actualizar precio.")
+        print ("4. Salir.")
+
+        print("ingrese la opcion que desea ")
+
+        opcion = 0
+
+        if opcion == "1" :
+            input (f"Ingrese la marca a consultar : {stock_marca} ")
+            print (f"El stock es {stock}")
+
+        if opcion == "2" :
+            input("Ingrese el precio mínimo : ")
+            input("Ingrese el precio máximo : ")
+            print("Los notebooks entre los precios consultas son : ")
+
+        if opcion == "3" :
+            input("Ingrese modelo a actualizar : ")
+            input("Ingrese precio nuevo : ")
+
+        if opcion == "4" :
+            print("Programa finalizado.")
+
+        else :
+            print("Debe seleccionar una opción válida!!")
+    
+main
